bbf39a488f77a24987304247cfcc41dbdcb34328
AndreyTerechenko <Dronka97@yandex.ru>
2024-03-17 09:22:56 +0300

    error correction

--- src/Main.java
+++ src/Main.java
@@ -7,37 +7,29 @@ public class Main {
         } else if (clientOs == 0) {
             System.out.println("становите версию приложения для iOS по ссылке");
         }
-
         //Задача 2
-        clientOs = 1;
         int clientDeviceYear = 2015;
         if (clientOs == 1 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченую версию приложения для Android по ссылке");
-        } else if (clientOs == 1 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        if (clientOs == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченую версию приложения для Ios по ссылке");
-        } else if (clientOs == 0 && clientDeviceYear >= 2015) {
-            System.out.println("становите версию приложения для iOS по ссылке");
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOs == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Ios по ссылке");
         }
         //задача 3
         int year = 2021;
-        if (year >= 1584 && year % 400 == 0 || year >= 1584 && year % 4 == 0 && year % 100 != 0) {
+        if (year >= 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
             System.out.println(year + " год является високосным");
         } else System.out.println(year + " год не является високосным");
-
         // задача 4
         int days = 0;
         int deliveryDistance = 95;
         if (deliveryDistance < 20) {
-            days = days + 1;
+            days += 1;
             System.out.println("Доставка займет " + days + " день");
         } else if (deliveryDistance < 60) {
-            days = days + 2;
+            days += 2;
             System.out.println("Доставка займет " + days + " дня");
         } else if (deliveryDistance < 100) {
-            days = days + 3;
+            days += 3;
             System.out.println("Доставка займет " + days + " дня");
         } else System.out.println("Доставка не осуществляется");
         //Задача 5
@@ -66,6 +58,10 @@ public class Main {
             default:
                 System.out.println("Ошибка");
                 //со свичом очень просто(понравлось)
+                //Учел все замечания, применил ctr+alt+L для форматирования кода. Про пропущенные пробелы немного не понял,
+                //Но пробежался по коду и неточности постарался исправить. Так же убрал избыточные условия во второй задаче.
+                //Жду коментарии и замечания. Спасибо.
+
         }
     }
 }
